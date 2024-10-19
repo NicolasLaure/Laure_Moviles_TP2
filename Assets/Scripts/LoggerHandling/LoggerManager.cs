f5a588d5b6b531664d6e23c14a6983328b1052fa
Nicodragon25 <nicolasariellaure@gmail.com>
2024-10-19 15:25:16 -0300

    Implement logger

--- Assets/Scripts/LoggerHandling/LoggerManager.cs
+++ Assets/Scripts/LoggerHandling/LoggerManager.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class LoggerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TextMeshProUGUI label;
+    
+    private const string pluginClassName = "com.laure.loggerplugin.LaureLogger";
+
+#if UNITY_ANDROID || UNITY_EDITOR
+    private AndroidJavaClass pluginClass;
+    private AndroidJavaObject pluginInstance;
+#endif
     void Start()
     {
-        
+        label.text = "Start";
+        Debug.Log("Unity - " + pluginClassName);
+#if UNITY_ANDROID || UNITY_EDITOR
+        pluginClass = new AndroidJavaClass(pluginClassName);
+        pluginInstance = pluginClass.CallStatic<AndroidJavaObject>("getInstance");
+#endif
     }
 
-    // Update is called once per frame
-    void Update()
+    [ContextMenu("SendLog")]
+    public void SendLog()
     {
-        
+        Debug.Log("Unity - SendLog");
+#if UNITY_ANDROID || UNITY_EDITOR
+        pluginInstance.Call("SendLog", Time.time.ToString());
+#endif
+    }
+
+    [ContextMenu("UpdateLog")]
+    public void UpdateLogs()
+    {
+        Debug.Log("Unity - UpdateLogs");
+#if UNITY_ANDROID || UNITY_EDITOR
+        label.text = pluginInstance.Call<string>("GetAllLogs");
+#endif
     }
 }
